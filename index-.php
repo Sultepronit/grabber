18ab5c3afb9b28a37bfc4eea2b06be3e0efce524
sultepronit <sultepronit@gmail.com>
2025-09-15 07:35:43 +0000

    preparing for merge

--- index-.php
+++ index-.php
@@ -9,31 +9,40 @@ require_once __DIR__ . '/app/services/curl.php';
 require_once __DIR__ . '/app/utils/isUkrainian.php';
 
 require_once __DIR__ . '/app/useE2u.php';
+require_once __DIR__ . '/app/geminiEn.php';
 require_once __DIR__ . '/app/useGlosbe.php';
-require_once __DIR__ . '/app/useJisho.php';
+require_once __DIR__ . '/app/processJisho.php';
 require_once __DIR__ . '/app/gtranslate.php';
 require_once __DIR__ . '/app/kanjiLookup.php';
+
+// echo '<pre>';
+// print_r($_SERVER);
+// print_r($_GET);
 
 try {
     $dic = $_GET['dic'] ?? '';
     $word = $_GET['word'] ?? '';
     // $dic = 'e2u';
-    // // $dic = 'glosbe';
+    // $dic = 'glosbe';
+    // $dic = 'gemEn';
     // // $dic = 'gtranslate';
-    // $dic = 'jisho';
-    // $dic = 'kanji-lookup';
     // $word = 'snake';
     // // $word = 'поміж нас';
-    // $word = '湖';
+    // $word = 'down the road';
+    // // $word = 'apple pie';
+    // $word = 'in my heart of hearts';
+    // // $word = 'give me a break';
+    // $word = 'if I was you I'd wanna be me too';
 
     if ($dic === 'e2u') {
         useE2u($word);
+    } else if ($dic === 'gem-en') {
+        useGeminiEn($word);
     } else if ($dic === 'glosbe') {
         useGlosbe($word);
     } else if ($dic === 'jisho') {
-        // $word = urlencode($word);
-        // $url = 'https://jisho.org/search/'.$word;
-        useJisho($word);
+        $word = urlencode($word);
+        $url = 'https://jisho.org/search/'.$word;
     } else if ($dic === 'gtranslate') {
         gtranslate($word);
     } else if ($dic === 'kanji-lookup') {
